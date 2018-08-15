256e755ab6d91085976ae003dc4cf7fc07defd43
Sean M <sean@arrowburn.co.nz>
2018-08-16 11:24:13 +1200

    implemented signin mutation on the client-side

--- client/src/components/Auth/Signin.js
+++ client/src/components/Auth/Signin.js
@@ -1,8 +1,80 @@
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import { SIGNIN_USER } from "../../queries";
+import Error from "../Error";
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  passwordConfirmation: ""
+};
 
 class Signin extends Component {
+  state = { ...initialState };
+
+  clearState = () => {
+    this.setState({ ...initialState });
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event, signinUser) => {
+    event.preventDefault();
+    signinUser().then(data => {
+      console.log(data);
+    });
+    this.clearState();
+  };
+
   render() {
-    return <div>Signin</div>;
+    const { username, password } = this.state;
+    return (
+      <div className="App container">
+        <h2>Signin</h2>
+        <Mutation mutation={SIGNIN_USER} variables={{ username, password }}>
+          {(signinUser, { data, loading, error }) => {
+            return (
+              <form
+                action=""
+                onSubmit={event => this.handleSubmit(event, signinUser)}
+              >
+                <div className="form-group">
+                  <input
+                    onChange={this.handleChange}
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    placeholder="Username"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    onChange={this.handleChange}
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="Password"
+                  />
+                </div>
+                <button
+                  // disabled={loading || this.validateForm()}
+                  type="submit"
+                  className="btn btn-lg btn-primary"
+                >
+                  Submit
+                </button>
+                {error && <Error error={error} />}
+              </form>
+            );
+          }}
+        </Mutation>
+      </div>
+    );
   }
 }
 
